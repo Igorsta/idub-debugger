a52434de15dc687f067c4c7a0eb9eefeaa853c5a
Igor Staszkiewicz <i.staszkiewi@student.uw.edu.pl>
2025-11-21 12:24:27 +0100

    moved exec declaration to separate header

--- vm/core/baby_vm.cpp
+++ vm/core/baby_vm.cpp
@@ -14,19 +14,12 @@
 #include <tuple>
 #include <type_traits>
 #include <unordered_map>
-
+#include "exec.hpp"
 #include "biject_map.hpp"
 
 ///////////////////// DECLARATIONS ////////////////////////////
 
-enum class exec_mode {
-	NORMAL,
-	DEBUG,
-};
 
-struct instrutction_t;
-struct frame_t;
-struct memory_space;
 struct thread_dbg_data_t;
 struct dbg_require_stop;
 
@@ -42,10 +35,8 @@ struct io_handler_t;
 
 ///////////////////// USINGS ////////////////////////////
 
-using unit = uint64_t;
 using code_block = std::vector<instrutction_t>;
 
-using func_id_t = uint64_t;
 using labl_id_t = uint64_t;
 using reg_id_t = uint64_t;
 using breakpoints_id = uint64_t;
@@ -56,26 +47,8 @@ using labl_id_map = biject_map_t<std::string, labl_id_t>;
 using labl_map = std::unordered_map<labl_id_t, size_t>;
 
 using func_id_map = biject_map_t<std::string, func_id_t>;
-using func_map = std::unordered_map<func_id_t, function_t>;
 
 ///////////////////// EXEC MACRO ////////////////////////////
-
-#define CONST_RAW_EXEC_ARGS                                                                        \
-	[[maybe_unused]] const instrutction_t *instr, [[maybe_unused]] const memory_space *mem_space,  \
-		[[maybe_unused]] const frame_t *frame, [[maybe_unused]] const func_map *avail_funcs
-
-#define _N_EXEC		  exec
-#define _N_EXEC_UTILS _N_EXEC::utils
-
-namespace _N_EXEC {
-#define RAW_EXEC_ARGS                                                                              \
-	[[maybe_unused]] const instrutction_t *&instr, [[maybe_unused]] memory_space *mem_space,       \
-		[[maybe_unused]] frame_t *&frame, [[maybe_unused]] func_map *avail_funcs
-#define FWD_RAW_ARGS instr, mem_space, frame, avail_funcs
-using ret_t = std::ptrdiff_t;
-using func_t = ret_t(RAW_EXEC_ARGS);
-
-}; // namespace _N_EXEC
 
 #define _N_PARSE	   parse
 #define _N_PARSE_UTILS _N_PARSE::utils
---
+++ vm/core/exec.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <unordered_map>
+
+struct instrutction_t;
+struct memory_space;
+struct frame_t;
+struct function_t;
+
+using func_id_t = uint64_t;
+using unit = uint64_t;
+
+using func_map = std::unordered_map<func_id_t, function_t>;
+
+#define CONST_RAW_EXEC_ARGS                                                                        \
+	[[maybe_unused]] const instrutction_t *instr, [[maybe_unused]] const memory_space *mem_space,  \
+		[[maybe_unused]] const frame_t *frame, [[maybe_unused]] const func_map *avail_funcs
+
+#define _N_EXEC		  exec
+#define _N_EXEC_UTILS _N_EXEC::utils
+
+namespace _N_EXEC {
+#define RAW_EXEC_ARGS                                                                              \
+	[[maybe_unused]] const instrutction_t *&instr, [[maybe_unused]] memory_space *mem_space,       \
+		[[maybe_unused]] frame_t *&frame, [[maybe_unused]] func_map *avail_funcs
+#define FWD_RAW_ARGS instr, mem_space, frame, avail_funcs
+using ret_t = std::ptrdiff_t;
+using func_t = ret_t(RAW_EXEC_ARGS);
+
+}; // namespace _N_EXEC
+
+#define _N_PARSE	   parse
+#define _N_PARSE_UTILS _N_PARSE::utils
